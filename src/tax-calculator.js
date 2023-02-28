80fcf9dbd86f09faae3c029da5a9b68df06c4ce4
benjaminbowyer <benjaminbo549@gmail.com>
2023-02-28 13:16:31 +0000

    Working version

--- src/tax-calculator.js
+++ src/tax-calculator.js
@@ -3,28 +3,39 @@ const higherRateMaxIncome = 50270;
 const additionalRateMaxIncome = 150000;
 
 const calculate = function (income) {
-  var taxBasic = (income - basicRateMaxIncome) * 0.2
-  console.log(`taxbasicinitial: ${taxBasic}`)
-  var taxHigher = (income - higherRateMaxIncome) * 0.4
-  console.log(`taxhigherinitial: ${taxHigher}`)
-  var taxAdditional = (income - additionalRateMaxIncome) * 0.45
-  console.log(`taxadditionalinitial: ${taxAdditional}`)
+  if (income < basicRateMaxIncome) {
+    return 0
+  }
 
-  if (taxBasic < 0) {
-    (taxBasic = 0)
-    console.log(`taxbasicfinal: ${taxBasic}`)
+  if (income < higherRateMaxIncome) {
+      var taxBasic = (income - basicRateMaxIncome) * 0.2
+      console.log(`taxbasicinitial: ${taxBasic}`)
+
+      var tax = taxBasic
+      return (tax)
+    }
+
+  if (income < additionalRateMaxIncome) {
+      var taxBasic = (37700) * 0.2
+      console.log(`taxbasicinitial: ${taxBasic}`)
+      var taxHigher = (income - higherRateMaxIncome) * 0.4
+      console.log(`taxhigherinitial: ${taxHigher}`)
+    
+      tax = taxBasic + taxHigher
+      return (tax)
   }
-  if (taxHigher < 0) {
-    (taxHigher = 0)
-    console.log(`taxhigherfinal: ${taxHigher}`)
-  }
-  if (taxAdditional < 0) {
-    (taxAdditional = 0)
-    console.log(`taxadditionalfinal: ${taxAdditional}`)
-}
-  
-  var tax = taxBasic + taxHigher + taxAdditional
-  return (tax)
-}
+
+  if (income > additionalRateMaxIncome) {
+      var taxBasic = (37700) * 0.2
+      console.log(`taxbasic: ${taxBasic}`)
+      var taxHigher = (99730) * 0.4
+      console.log(`taxhigher: ${taxHigher}`)
+      var taxAdditional = (income - additionalRateMaxIncome) * 0.45
+      console.log(`taxadditional: ${taxAdditional}`)
+
+      tax = taxBasic + taxHigher + taxAdditional
+      return (tax)
+      }
+} 
 
 module.exports.calculate = calculate;
