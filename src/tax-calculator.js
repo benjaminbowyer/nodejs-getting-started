c3da84c9e50234aa6ca28f4d62c0663115dbeb8c
benjaminbowyer <benjaminbo549@gmail.com>
2023-02-28 11:25:35 +0000

    This version doesnt work

--- src/tax-calculator.js
+++ src/tax-calculator.js
@@ -3,31 +3,28 @@ const higherRateMaxIncome = 50270;
 const additionalRateMaxIncome = 150000;
 
 const calculate = function (income) {
-  if (income < basicRateMaxIncome) {
-    return 0
-  } else {
-    if (income < higherRateMaxIncome) {
-      return (income - basicRateMaxIncome) * 0.2
-    } else {
-      if (income < additionalRateMaxIncome) {
-        var tax1 = (37700) * 0.2
-        console.log(`tax 1: ${tax1}`)
-        var tax2 = (income - higherRateMaxIncome) * 0.4
-        console.log(`tax 2: ${tax2}`)
-        tax = tax1 + tax2
-        return tax
-      } else {
-      var tax1 = (37700) * 0.2
-      var tax2 = (99730) * 0.4
-      var tax3 = (income - additionalRateMaxIncome) * 0.45
-      tax = tax1 + tax2 + tax3
-      return tax
-    }
- 
+  var taxBasic = (income - basicRateMaxIncome) * 0.2
+  console.log(`taxbasicinitial: ${taxBasic}`)
+  var taxHigher = (income - higherRateMaxIncome) * 0.4
+  console.log(`taxhigherinitial: ${taxHigher}`)
+  var taxAdditional = (income - additionalRateMaxIncome) * 0.45
+  console.log(`taxadditionalinitial: ${taxAdditional}`)
+
+  if (taxBasic < 0) {
+    (taxBasic = 0)
+    console.log(`taxbasicfinal: ${taxBasic}`)
   }
-}
+  if (taxHigher < 0) {
+    (taxHigher = 0)
+    console.log(`taxhigherfinal: ${taxHigher}`)
+  }
+  if (taxAdditional < 0) {
+    (taxAdditional = 0)
+    console.log(`taxadditionalfinal: ${taxAdditional}`)
 }
   
-
+  var tax = taxBasic + taxHigher + taxAdditional
+  return (tax)
+}
 
 module.exports.calculate = calculate;
